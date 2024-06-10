5a94395731716bb56892e247c374f014db854f54
tegra10 <tegkit30@gmail.com>
2024-06-10 23:32:30 +0100

    crud complet

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -28,3 +28,52 @@ module.exports.setPosts = async (req, res) => {
         console.error(err);
     }
 };
+module.exports.editPosts = async (req, res) => {
+    const userId = req.params.id;
+    const { name } = req.body;
+    postModel
+        .findByPk(userId)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ error: "Utilisateur non trouvé" });
+            } else {
+                user.name = name;
+
+                return user.save();
+            }
+        })
+        .then(updatedUser => {
+            res.json(updatedUser);
+        })
+        .catch(err => {
+            console.error(
+                "Erreur lors de la mise à jour de l'utilisateur :",
+                err
+            );
+            res.status(500).json({
+                error: "Erreur lors de la mise à jour de l'utilisateur"
+            });
+        });
+};
+
+module.exports.deletePosts = async (req, res) => {
+    const userId = req.params.id;
+    postModel
+        .findByPk(userId)
+        .then(user => {
+            if (!user) {
+                res.status(400).json({ message: `l'id n'est pas connu` });
+            } else {
+                return user.destroy();
+            }
+        })
+        .then(() => {
+            res.status(200).json({
+                message: `l'id nº ${userId} a bien était supprimé`
+            });
+        }).catch(err=>{
+          res.status(500).json({message:`l'erreur est ${err}`})
+          console.error(err)
+        })
+        
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,16 @@
 "use strict";
 const express = require("express");
 const Router = express.Router();
-const { getPosts, setPosts } = require("../controllers/post.controller.js");
+const {
+    getPosts,
+    setPosts,
+    editPosts,
+    deletePosts
+} = require("../controllers/post.controller.js");
 
 Router.get("/", getPosts);
 Router.post("/", setPosts);
+Router.put("/:id", editPosts);
+Router.delete("/:id", deletePosts);
 
 module.exports = Router;
