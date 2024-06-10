8a5fc2c192f1785f370f106e59c9f1c678e126f2
tegra10 <tegkit30@gmail.com>
2024-06-10 20:21:33 +0100

    commit avec la methode post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -9,6 +9,22 @@ module.exports.getPosts = async (req, res) => {
         res.status(200).json(users);
     } catch (err) {
         res.status(500).json({ message: err });
-        console.log('l\'erreur est ' + err);
+        console.log("l'erreur est " + err);
+    }
+};
+module.exports.setPosts = async (req, res) => {
+   const {name,email}=req.body
+    try {
+        if (!name) {
+            res.status(400).json({ message: "Merci d'ajouter un message" });
+        }
+
+        const post = await postModel.create({
+            name,
+            email
+        });
+        res.status(201).json(post);
+    } catch (err) {
+        console.error(err);
     }
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,9 @@
 "use strict";
 const express = require("express");
 const Router = express.Router();
-const {
-    getPosts,
-} = require("../controllers/post.controller.js");
+const { getPosts, setPosts } = require("../controllers/post.controller.js");
 
 Router.get("/", getPosts);
+Router.post("/", setPosts);
 
-
-module.exports= Router
+module.exports = Router;
